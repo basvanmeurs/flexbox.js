e8799e274ec25b57691e293f8ee644f2f9237de3
Bas van Meurs <bas@planning.nl>
2020-03-12 15:49:12 +0100

    chore(package): add index file with relevant types

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import FlexTarget from './FlexTarget';
+import FlexSubject from './FlexSubject';
+import FlexItem from './FlexItem';
+import FlexContainer from './FlexContainer';
+import FlexNode from './FlexNode';
+
+export { FlexNode, FlexSubject, FlexTarget, FlexItem, FlexContainer };
